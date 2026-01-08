daa83da568f162655e96caf642fa0fd197841da9
Andrew Nicoll <anicoll01@gmail.com>
2026-01-08 15:49:17 +1030

    fix: infer type pointer

--- testdata/testmod/domain/user.go
+++ testdata/testmod/domain/user.go
@@ -6,5 +6,6 @@ type User struct {
 	ID        string
 	Name      string
 	UpdatedAt time.Time
+	DeletedAt *time.Time
 	Address   *Address
 }
--- testdata/testmod/model/user.go
+++ testdata/testmod/model/user.go
@@ -4,5 +4,6 @@ type UserModel struct {
 	ID        string
 	Name      string
 	UpdatedAt string
+	DeletedAt *string
 	Address   *AddressModel
 }
